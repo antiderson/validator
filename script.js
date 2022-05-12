aa9988f6af184a0a0b3e100f9a4aa610605f6037
Anderson Daniel <andersondaniel.adfa@gmail.com>
2022-05-12 13:52:09 -0300

    validação concluida

--- script.js
+++ script.js
@@ -1,55 +1,122 @@
 let usuarios = []
 let myform = document.getElementById('formulario')
-function verificar() {
-    var nome = document.querySelector('#nome')
-    var user = (nome.value)
+let nome = document.querySelector('#nome')
+let email = document.querySelector('#email')
+let senha = document.querySelector('#password')
+let rsenha = document.querySelector('#rpassword')
+let btn = document.querySelector('#check');
+btn.disabled = true;
+btn.classList.remove('btnNormal');
+btn.classList.add('btnDisabled');
 
-    var cont = document.querySelector('#email')
-    var email = (cont.value)
-    var senha = window.document.getElementById('password')
-    var pssw = (senha.value)
-    var senha2 = window.document.getElementById('rpassword')
-    var rpssw = (senha2.value)
-
-    // if (user == "" || user.length < 3 || user.length > 25) {
-    //     document.getElementById('nome').style.borderColor = 'red'
-    // } else {
-    //     document.getElementById('nome').style.borderColor = 'green'
-    // }
-
-
-    //senha diferente
-    if (pssw != rpssw) {
-        document.getElementById('password').style.borderColor = 'red'
-
-    }
-
-    
-    // //desabilita botao
-    // if (user == "" || pssw == "" || rpssw == "") {
-    //     //document.getElementById('check').disabled = true;
-    //     document.getElementById('check').classList.remove('btnNormal');
-    //     document.getElementById('check').classList.add('btnDisabled');
-    // }
-
-
-    //verifica usuario e email no array
-    let usuario = {
-        username: user,
-        email: email,
-        senha: pssw,
-    }
-    if (!usuarios.some(x => x.username == usuario.user || x.email == usuario.email)) {
-        usuarios.push(usuario)
-        console.log(usuarios)
-        myform.reset()
-    } else {
-        alert("ja existe")
-    }
-
-
-
+function mudaSpan(campo, msg) {
+    document.getElementById(`u${campo}`).style.color = msg;
 }
-function mudaCor() {
-    document.getElementById('nome').style.borderColor = 'red'
+function mudaCor(campo, cor) {
+    document.getElementById(`${campo}`).style.borderColor = cor;
+}
+
+function fnCheckBtn() {
+    let isValid = true;
+    if (nome.value == "" || nome.value.length < 3 || nome.value.length > 25) isValid = false;
+
+    let emailrejex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!emailrejex.test(email.value)) isValid = false;
+
+    if (senha.value.length < 8) isValid = false;
+
+    if (rsenha.value != senha.value) isValid = false;
+
+    if (isValid) {
+        btn.disabled = false;
+        btn.classList.remove('btnDisabled');
+        btn.classList.add('btnNormal');
+    } else {
+        btn.disabled = true;
+        btn.classList.remove('btnNormal');
+        btn.classList.add('btnDisabled');
+    }
+}
+
+nome.addEventListener('blur', ({ target }) => {
+    if (target.value == "" || target.value.length < 3 || target.value.length > 25) {
+        mudaCor(target.id, 'red')
+        mudaSpan(target.id, 'red')
+    } else {
+        mudaCor(target.id, 'green')
+        mudaSpan(target.id, 'rgba(0, 0, 0, 0.0)')
+    }
+    fnCheckBtn();
+})
+
+email.addEventListener('blur', ({ target }) => {
+    let emailrejex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!emailrejex.test(target.value)) {
+        mudaCor(target.id, 'red')
+        mudaSpan(target.id, 'red')
+    } else {
+        mudaCor(target.id, 'green')
+        mudaSpan(target.id, 'rgba(0, 0, 0, 0.0)')
+    }
+    fnCheckBtn();
+})
+
+senha.addEventListener('blur', ({ target }) => {
+    if (target.value.length < 8) {
+        mudaCor(target.id, 'red')
+        mudaSpan(target.id, 'red')
+    } else {
+        mudaCor(target.id, 'green')
+        mudaSpan(target.id, 'rgba(0, 0, 0, 0.0)')
+    }
+    fnCheckBtn();
+})
+
+rsenha.addEventListener('blur', ({ target }) => {
+    if (senha.value != target.value) {
+        mudaCor(target.id, 'red')
+        mudaSpan(target.id, 'red')
+    } else {
+        mudaCor(target.id, 'green')
+        mudaSpan(target.id, 'rgba(0, 0, 0, 0.0)')
+    }
+    fnCheckBtn();
+})
+
+function verificar() {
+    let usuario = {
+        username: nome.value,
+        email: email.value,
+        senha: senha.value,
+    }
+    let valido = true;
+
+    if (usuarios.some(x => x.email == usuario.email)) {
+        valido = false;
+        document.getElementById('uemail').innerText = "Email ja utilizado"
+        mudaCor('email', 'red')
+        mudaSpan('email', 'red')
+    }
+
+    if (usuarios.some(x => x.username == usuario.username)) {
+        valido = false;
+        document.getElementById('unome').innerText = "nome de usuario ja utilizado"
+        mudaCor('nome', 'red')
+        mudaSpan('nome', 'red')
+    }
+
+    if (valido) {
+        usuarios.push(usuario)
+        myform.reset()
+    }
+}
+
+btn.addEventListener('click', debounce(verificar, 5000))
+
+function debounce(func, wait) {
+	let timer = null;
+	return function() {
+		clearTimeout(timer);
+		timer = setTimeout(func, wait);
+	}
 }
